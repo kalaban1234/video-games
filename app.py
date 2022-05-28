ce8ad4dea94711773a1ebe3b3f4588f270c5894b
kalaban1234 <53945970+kalaban1234@users.noreply.github.com>
2022-05-28 21:37:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,113 @@
 import dash
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
 from dash import dcc
 from dash import html
-import plotly.graph_objects as go
-import pandas as pd
-import numpy as np
 
 # Dataset 'Processing'
+from plotly.offline import iplot
+#
+df = pd.read_csv('https://raw.githubusercontent.com/kalaban1234/video-games/main/video_games_sales.csv')
+# df['User_Score'] = df.User_Score.astype('float')
+# df['Year_of_Release'] = df.Year_of_Release.astype('int')
+# df['User_Count'] = df.User_Count.astype('int')
+# df['Critic_Count'] = df.Critic_Count.astype('int')
 
-df_emissions = pd.read_csv('emission_full.csv')
+# Building Graphs
+#Let's build a sales chart for video games in different countries depending on the year.
 
-df_emission_0 = df_emissions.loc[df_emissions['year']==2000]
+#Let's build a line plot with the dynamics of the number of released games and their sales by year.
+global_sales_years_df = df.groupby('Year_of_Release')[['Global_Sales']].sum()
+released_years_df = df.groupby('Year_of_Release')[['Name']].count()
+years_df = global_sales_years_df.join(released_years_df)
+years_df.columns = ['Global_Sales', 'Number_of_Games']
+trace0 = go.Scatter(
+    x=years_df.index,
+    y=years_df.Global_Sales,
+    name='Global Sales'
+)
 
-# Building our Graphs (nothing new here)
+trace1 = go.Scatter(
+    x=years_df.index,
+    y=years_df.Number_of_Games,
+    name='Number of games released'
+)
 
-data_choropleth = dict(type='choropleth',
-                       locations=df_emission_0['country_name'],  #There are three ways to 'merge' your data with the data pre embedded in the map
-                       locationmode='country names',
-                       z=np.log(df_emission_0['CO2_emissions']),
-                       text=df_emission_0['country_name'],
-                       colorscale='inferno',
-                       colorbar=dict(title='CO2 Emissions log scaled')
-                      )
+data = [trace0, trace1]
+layout = {'title': 'Statistics of video games'}
 
-layout_choropleth = dict(geo=dict(scope='world',  #default
-                                  projection=dict(type='orthographic'
-                                                 ),
-                                  #showland=True,   # default = True
-                                  landcolor='black',
-                                  lakecolor='white',
-                                  showocean=True,   # default = False
-                                  oceancolor='azure'
-                                 ),
-                         
-                         title=dict(text='World Choropleth Map',
-                                    x=.5 # Title relative position according to the xaxis, range (0,1)
-                                   )
-                        )
+fig1 = go.Figure(data=data, layout=layout)
 
-fig = go.Figure(data=data_choropleth, layout=layout_choropleth)
+#Let's build a bar chart that depict the market share of gaming platforms,
+#calculated by the number of games released and by total revenue.
+global_sales_platforms_df = df.groupby('Platform')[['Global_Sales']].sum()
+released_platforms_df = df.groupby('Platform')[['Name']].count()
+platforms_df = global_sales_platforms_df.join(released_platforms_df)
+
+platforms_df.columns = ['Global_Sales', 'Number_of_Games']
+platforms_df.sort_values('Global_Sales', inplace=True)
+platforms_df = platforms_df.apply(lambda x: 100*x/platforms_df.sum(), axis = 1)
+
+trace0 = go.Bar(
+    x=platforms_df.index,
+    y=platforms_df.Global_Sales,
+    name='Global Sales',
+    orientation = 'v'
+)
+
+trace1 = go.Bar(
+    x=platforms_df.index,
+    y=platforms_df.Number_of_Games,
+    name='Number of games released',
+    orientation = 'v'
+)
+
+data = [trace0, trace1]
+layout = {'title': 'Platforms share'}
+
+fig2 = go.Figure(data=data, layout=layout)
+#Let's look at the differences in critics'
+# ratings depending on the genre of the game.
+# data = []
+#
+# for genre in df.Genre.unique():
+#     data.append(
+#         go.Box(y=df[df.Genre == genre].Critic_Score,
+#                name=genre)
+#     )
+# fig3 = iplot(data, show_link = False)
+
+#Let's plot the dependence of the average user rating and critics' rating by genre
+# scores_genres_df = df.groupby('Genre')[['Critic_Score', 'User_Score']].mean()
+# sales_genres_df = df.groupby('Genre')[['Global_Sales']].sum()
+#
+# genres_df = scores_genres_df.join(sales_genres_df)
+#
+# trace0 = go.Scatter(
+#     x=genres_df.Critic_Score,
+#     y=genres_df.User_Score,
+#     mode = 'markers+text',
+#     text = genres_df.index,
+#     textposition='bottom center',
+#     marker = dict(
+#         size = 1/10*genres_df.Global_Sales,
+#         color = [
+#             'aqua', 'azure', 'beige', 'lightgreen',
+#             'lavender', 'lightblue', 'pink', 'salmon',
+#             'wheat', 'ivory', 'silver'
+#         ]
+#     )
+# )
+#
+# data = [trace0]
+# layout = {
+#     'title': 'Statistics of video games genres',
+#     'xaxis': {'title': 'Critic Score'},
+#     'yaxis': {'title': 'User Score'}
+# }
+#
+# fig3 = go.Figure(data=data, layout=layout)
 
 
 
@@ -51,16 +121,39 @@ server = app.server
 
 
 app.layout = html.Div(children=[
-    html.H1(children='My First DashBoard'),
+    html.H1(children='Video Game Sales with Ratings'),
 
     html.Div(children='''
-        Example of html Container
-    '''),
+        Statistic of video games
+    '''
+
+             ),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='example-graph1',
+        figure=fig1
+    ),
+    html.Div(children='''
+        Platforms share
+    '''
+
+             ),
+
+    dcc.Graph(
+        id='example-graph2',
+        figure=fig2
     )
+#     html.Div(children='''
+#     Statistic of genres
+# '''
+#
+#              ),
+#
+#     dcc.Graph(
+#         id='example-graph3',
+#         figure=fig3
+#     )
+
 ])
 
 
